4486d8181743d4823d117aa09f939d6bffa6525d
Troy Hill <thill@snowfall.local>
2014-03-20 22:16:36 -0600

    [TH CS] find mailing and create mailing

--- src/usps_processor/core.clj
+++ src/usps_processor/core.clj
@@ -29,3 +29,60 @@
       :content
       io/reader
       parsed-data))
+
+(defn find-mailing
+  [scan-data]
+  (first (d/match-entities (d/db) {:mailing/serial-number-6
+                                  (get-in scan-data [:imb-data
+                                                     :9-digit-mailer
+                                                     :serial-number])
+                                 :mailing/serial-number-9
+                                  (get-in scan-data [:imb-data
+                                                     :6-digit-mailer
+                                                     :serial-number])
+                                 :mailing/mailer-id-6
+                                  (get-in scan-data [:imb-data
+                                                     :6-digit-mailer
+                                                     :mailer])
+                                 :mailing/mailer-id-9
+                                  (get-in scan-data [:imb-data
+                                                     :9-digit-mailer
+                                                     :mailer])
+                                 :mailing/customer-number
+                                   (get-in scan-data [:imb-data
+                                                      :customer-number])
+                                 :mailing/routing-code
+                                  (:routing scan-data)})))
+
+(defn scan-data->scan-tx-data
+  [scan-data mailing-id]
+  [{:scan/mailing mailing-id
+    :scan/facility-zip (:facility-zip scan-data)
+    :scan/operation-code (:operation-code scan-data)
+    :scan/time (:scan-time scan-data)
+    :scan/barcode (get-in scan-data [:imb-data :barcode])
+    :scan/service (get-in scan-data [:imb-data :service])}])
+
+(defn scan-data->mailing-tx-data
+  [scan-data]
+  [{:db/id (d/tempid)
+    :mailing/serial-number-6
+    (get-in scan-data [:imb-data :6-digit-mailer :mailer])
+
+    :mailing/mailer-id-9
+    (get-in scan-data [:imb-data :6-digit-mailer :serial-number])
+
+    :mailing/serial-number-9
+    (get-in scan-data [:imb-data :9-digit-mailer :mailer])
+
+    :mailing/mailer-id-6
+    (get-in scan-data [:imb-data :9-digit-mailer :serial-number])
+
+    :mailing/customer-number
+    (get-in scan-data [:imb-data :customer-number])}])
+
+(defn store-usps-data
+  [scan-data]
+  (let [mailing (or (find-mailing scan-data)
+                    (scan-data->mailing-tx-data scan-data))]
+    (d/transact (scan-data->scan-tx-data (:db/id mailing)))))
