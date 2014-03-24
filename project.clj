78e3eb9b856b3d9dac3b39c9117a69aebe7d0616
Chris Shea <cmshea@gmail.com>
2014-03-24 16:59:50 -0400

    [CS] Logging

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [org.slf4j/slf4j-simple "1.7.6"]
                  [turbovote.resource-config "0.1.1"]
                  [turbovote.datomic-toolbox "0.1.0-SNAPSHOT"]
                  [com.datomic/datomic-pro "0.9.4609" :exclusions [org.slf4j/slf4j-nop]]
--- src/usps_processor/core.clj
+++ src/usps_processor/core.clj
@@ -2,13 +2,18 @@
   (:require [usps-processor.s3 :as s3]
             [usps-processor.parse :as parse]
             [usps-processor.db :as db]
-            [usps-processor.sqs :as sqs]))
+            [usps-processor.sqs :as sqs]
+            [clojure.tools.logging :refer [info]]))
 
 (defn process-file [s3-key]
+  (info "Processing" s3-key)
   (-> s3-key
       s3/reader-from-s3
       parse/parse
-      db/store-all))
+      db/store-all)
+  (info "Processed" s3-key))
 
 (defn -main [& args]
-  (sqs/consume-messages process-file))
+  (info "Starting up...")
+  (sqs/consume-messages process-file)
+  (info "Started"))
