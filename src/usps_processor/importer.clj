9dac2814bfae64b20ffe90252b8e9066dd30c835
Nathan Herzing <nherzing@gmail.com>
2014-04-30 10:29:41 -0400

    [NH] Send event to riemann on scan processing completion

--- src/usps_processor/importer.clj
+++ src/usps_processor/importer.clj
@@ -3,17 +3,29 @@
             [usps-processor.parse :as parse]
             [usps-processor.db :as db]
             [usps-processor.sqs :as sqs]
-            [clojure.tools.logging :refer [info]])
+            [clojure.tools.logging :refer [info]]
+            [turbovote.resource-config :refer [config]]
+            [riemann.client :as riemann])
   (:gen-class))
+
+(defn send-event [metric]
+  (when (config :riemann :host)
+    (let [c (riemann/udp-client :host (config :riemann :host) :port (config :riemann :port))]
+      (riemann/send-event c {:service "usps-processor scans" :metric metric
+                             :tags ["usps-processor"]
+                             :description (str "Processed " metric " scans from "
+                                               (config :aws :s3 :bucket))}
+                          false))))
 
 (defn process-file [message]
   (let [s3-key (:body message)]
     (info "Processing" s3-key)
-    (-> s3-key
-        s3/reader-from-s3
-        parse/parse
-        db/store-scans)
-    (info "Processed" s3-key)))
+    (let [scans (-> s3-key
+                    s3/reader-from-s3
+                    parse/parse)]
+      (db/store-scans scans)
+      (send-event (count scans))
+      (info "Processed" s3-key))))
 
 (defn -main [& args]
   (info "Starting up...")
