6e709682b8ef24e76547a58cdd7f345b0a71bfb1
Wes Morgan <cap10morgan@gmail.com>
2015-04-07 20:33:12 -0600

    [WM] update dependencies to latest releases

--- project.clj
+++ project.clj
@@ -4,24 +4,24 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [org.slf4j/slf4j-simple "1.7.6"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.slf4j/slf4j-simple "1.7.12"]
                  [turbovote.resource-config "0.1.4"]
                  [turbovote.datomic-toolbox "0.2.5-SNAPSHOT" :exclusions [com.datomic/datomic-pro]]
                  [com.datomic/datomic-pro "0.9.5130" :exclusions [org.slf4j/slf4j-nop]]
-                 [clj-aws-s3 "0.3.8"]
+                 [clj-aws-s3 "0.3.10"]
                  [clj-time "0.9.0"]
                  [democracyworks.squishy "1.0.0"]
                  [org.clojure/data.csv "0.1.2"]
-                 [turbovote.imbarcode "0.1.4-SNAPSHOT"
+                 [turbovote.imbarcode "0.1.5"
                   :exclusions [org.clojure/clojure]]
-                 [compojure "1.1.6"]
+                 [compojure "1.3.3"]
                  [ring/ring-core "1.3.2"]
                  [javax.servlet/servlet-api "2.5"]
                  [org.immutant/web "2.0.0-beta2"]
-                 [riemann-clojure-client "0.2.9"]
-                 [com.novemberain/langohr "3.0.1"]]
-  :plugins [[lein-immutant "2.0.0-beta1"]]
+                 [riemann-clojure-client "0.3.2"]
+                 [com.novemberain/langohr "3.1.0"]]
+  :plugins [[lein-immutant "2.0.0"]]
   :profiles {:dev {:resource-paths ["dev-resources"]
                    :source-paths ["dev-src"]}
              :test {:resource-paths ["test-resources"]
