6f5ec8f29632f9825db019ea64eaab1c2e82dafd
Wes Morgan <cap10morgan@gmail.com>
2015-04-24 10:33:07 -0600

    [WM] update immutant to 2.0.0

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
                  [compojure "1.3.3"]
                  [ring/ring-core "1.3.2"]
                  [javax.servlet/servlet-api "2.5"]
-                 [org.immutant/web "2.0.0-beta2"]
+                 [org.immutant/web "2.0.0"]
                  [riemann-clojure-client "0.3.2"]
                  [com.novemberain/langohr "3.1.0"]]
   :plugins [[lein-immutant "2.0.0"]]
