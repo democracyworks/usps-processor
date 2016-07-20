17129fd40bd78fb4c8e428d2d4a15a764e762ae3
Troy Hill <troy@democracy.works>
2016-07-20 16:38:38 -0600

    Much better scan line parsing/validation
    Bad scan line shouldn't stop processing anymore
    Used schemas to validate data

--- src/usps_processor/parse.clj
+++ src/usps_processor/parse.clj
@@ -1,12 +1,13 @@
 (ns usps-processor.parse
-  (:require [clojure.string :as s]
+  (:require [clojure.string :as str]
             [clojure.data.csv :as csv]
             [clojure.tools.logging :as log]
             [turbovote.imbarcode :as imb]
             [clj-time.core :as time]
             [clj-time.format :as format]
             [clj-time.coerce :as coerce]
-            [usps-processor.zip-lookup :as zip]))
+            [usps-processor.zip-lookup :as zip]
+            [schema.core :as s]))
 
 (def default-timezone-id "America/Los_Angeles")
 (def usps-time-format-pattern "M/d/yyyy H:mm:ss")
@@ -33,24 +34,117 @@
   "Takes the time in the USPS string format, and a timezone id like
    'America/New_York', and returns a DateTime in UTC for that time."
   [time-string timezone-id]
-  (let [timezone (timezone-id->timezone timezone-id)
-        formatter (format/formatter usps-time-format-pattern timezone)
-        time-in-zone (format/parse formatter time-string)]
-    (time/to-time-zone time-in-zone time/utc)))
+  (try
+    (let [timezone (timezone-id->timezone timezone-id)
+          formatter (format/formatter usps-time-format-pattern timezone)
+          time-in-zone (format/parse formatter time-string)]
+      (time/to-time-zone time-in-zone time/utc))
+    (catch Exception ex
+      (log/warn "Exception parsing scan time" ex))))
+
+(defn datetime->date
+  "Converts a datetime to date, or returns nil if datetime is nil"
+  [datetime]
+  (when datetime
+    (coerce/to-date datetime)))
+
+(defn ->imb-data
+  "Combines the structure-digits and routing code and
+   splits them into the possible mailer-ids and
+   serial numbers"
+  [structure-digits routing-code]
+  (when (and structure-digits
+             (= 20 (count structure-digits)))
+    (imb/split-structure-digits
+     (str structure-digits routing-code))))
+
+(defn valid-number?
+  [count number]
+  (let [pattern (str "\\d{" count "}?")
+        regex (re-pattern pattern)]
+    (not (nil? (re-matches regex number)))))
+
+(defn valid-barcode?
+  [zip]
+  (valid-number? 2 zip))
+
+(defn valid-facility-zip?
+  [zip]
+  (valid-number? 5 zip))
+
+(defn valid-scan-time?
+  [scan-time]
+  (isa? (class scan-time) java.util.Date))
+
+(defn valid-customer-number?
+  [customer-number]
+  (valid-number? 15 customer-number))
+
+(defn valid-number-3?
+  [number]
+  (valid-number? 3 number))
+
+(defn valid-number-6?
+  [number]
+  (valid-number? 6 number))
+
+(defn valid-number-9?
+  [number]
+  (valid-number? 9 number))
+
+(defn valid-routing-number?
+  [number]
+  (and (not (nil? number))
+       (not (nil? (re-matches #"\d*?" number)))
+       (#{0 5 9 11} (count number))))
+
+(def inbound-schema
+  {:barcode (s/pred valid-barcode?)
+   :service (s/pred valid-number-3?)
+   :routing s/Str
+   :customer-number (s/pred valid-customer-number?)})
+
+(def outbound-schema
+  {:barcode (s/pred valid-barcode?)
+   :service (s/pred valid-number-3?)
+   :routing s/Str
+   :9-digit-mailer
+   {:mailer-id (s/pred valid-number-9?)
+    :serial-number (s/pred valid-number-6?)}
+   :6-digit-mailer
+   {:mailer-id (s/pred valid-number-6?)
+    :serial-number (s/pred valid-number-9?)}})
+
+(def scan-schema
+  {:facility-zip (s/pred valid-facility-zip?)
+   :operation-code (s/pred valid-number-3?)
+   :scan-time (s/pred valid-scan-time?)
+   :timezone-id s/Str
+   :imb-data (s/conditional :customer-number
+                            inbound-schema
+                            :9-digit-mailer
+                            outbound-schema)})
 
 (defn row->map [row]
   (let [[facility op-code time routing-code structure-digits] row
         timezone-id (zip->timezone-id facility)
         time-utc (->time-utc time timezone-id)
-        routing-code (s/trim routing-code)]
-    {:facility-zip facility
-     :operation-code op-code
-     :scan-time (coerce/to-date time-utc)
-     :timezone-id timezone-id
-     :imb-data (imb/split-structure-digits
-                (str structure-digits routing-code))}))
+        scan-time (datetime->date time-utc)
+        routing-code (when routing-code (str/trim routing-code))
+        imb-data (->imb-data structure-digits routing-code)
+        row-data {:facility-zip facility
+                  :operation-code op-code
+                  :scan-time scan-time
+                  :timezone-id timezone-id
+                  :imb-data imb-data}]
+    (try
+      (s/validate scan-schema row-data)
+      (catch Exception ex
+        (log/error "Scan line didn't process: " row)
+        (log/error "  Reason: " ex)))))
 
 (defn parse [input]
   (->> input
        csv/read-csv
-       (map row->map)))
+       (map row->map)
+       (remove nil?)))
--- test/usps_processor/parse_test.clj
+++ test/usps_processor/parse_test.clj
@@ -27,3 +27,73 @@
   (testing "defaults to LA time if the time zone id isn't a known one"
     (is (= (time/date-time 2016 4 7 21 58 0 0))
         (->time-utc "04/07/2016 13:58:00" "America/Walla_Walla"))))
+
+(deftest valid-number?-test
+  (testing "passes when all digits and match count"
+    (is (valid-number? 5 "12345")))
+  (testing "fails when not all digits"
+    (is (not (valid-number? 5 "a2345"))))
+  (testing "fails when count isn't met"
+    (is (not (valid-number? 5 "1234")))
+    (is (not (valid-number? 5 "123456")))))
+
+(deftest valid-scan-time?-test
+  (testing "passes with a java.util.Date"
+    (is (valid-scan-time? (java.util.Date.))))
+  (testing "fails with nil"
+    (is (not (valid-scan-time? nil)))))
+
+(deftest valid-routing-number?-test
+  (testing "passes with blank"
+    (is (valid-routing-number? "")))
+  (testing "passes with 5 digits"
+    (is (valid-routing-number? "12345")))
+  (testing "passes with 9 digits"
+    (is (valid-routing-number? "123456789")))
+  (testing "passes with 11 digits"
+    (is (valid-routing-number? "12345678901")))
+  (testing "fail cases"
+    (is (not (valid-routing-number? "12")))
+    (is (not (valid-routing-number? "a1234")))
+    (is (not (valid-routing-number? nil)))))
+
+(deftest row->map-test
+  (testing "valid outbound row, blank routing code"
+    (let [data (row->map ["80204" "141" "07/20/2016 12:38:20"
+                          " " "00040123456789123456"])]
+      (is (= "123456789" (get-in data [:imb-data :9-digit-mailer :mailer-id])))
+      (is (= "123456" (get-in data [:imb-data :6-digit-mailer :mailer-id])))
+      (is (nil? (get-in data [:imb-data :customer-number])))
+      (is (= "00" (get-in data [:imb-data :barcode])))
+      (is (= "" (get-in data [:imb-data :routing])))
+      (is (= "America/Denver" (:timezone-id data)))))
+  (testing "valid outbound row, 5 digit routing code"
+    (let [data (row->map ["80204" "141" "07/20/2016 12:38:20"
+                          "80204      " "00040123456789123456"])]
+      (is (= "80204" (get-in data [:imb-data :routing])))))
+  (testing "valid inbound row, 11 digit routing code"
+    (let [data (row->map ["80204" "141" "07/20/2016 12:38:20"
+                          "80204123456" "00050123456789123456"])]
+      (is (= "123456789123456" (get-in data [:imb-data :customer-number])))
+      (is (nil? (get-in data [:imb-data :9-digit-mailer :mailer-id])))
+      (is (nil? (get-in data [:imb-data :6-digit-mailer :mailer-id])))
+      (is (= "00" (get-in data [:imb-data :barcode])))
+      (is (= "80204123456" (get-in data [:imb-data :routing])))
+      (is (= "America/Denver" (:timezone-id data)))))
+  (testing "mangled time"
+    (let [data (row->map ["80204" "141" "037/20/2016 12:38:20"
+                          "80204123456" "00050123456789123456"])]
+      (is (nil? data))))
+  (testing "mangled structured digits"
+    (let [data (row->map ["80204" "141" "037/20/2016 12:38:20"
+                          "80204123456" "000501"])]
+      (is (nil? data)))
+    (let [data (row->map ["80204" "141" "07/20/2016 12:38:20"
+                          "80204123456" "000501234567891234567"])]
+      (is (nil? data))))
+  (testing "mangled row"
+    (let [data (row->map ["8020"])]
+      (is (nil? data))))
+  (testing "empty row"
+    (let [data (row->map [])]
+      (is (nil? data)))))
