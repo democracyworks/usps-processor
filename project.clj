679a22db76746df47e6dfabf58a6c9afcd9368d2
Troy Hill <troy@democracy.works>
2016-08-03 16:47:05 -0700

    modernizing WIP

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/core.async "0.2.385"]
                  [org.slf4j/slf4j-simple "1.7.21"]
                  [turbovote.resource-config "0.2.0"]
                  [democracyworks/datomic-toolbox "2.0.1"
@@ -28,6 +29,7 @@
                  [turbovote.imbarcode "0.1.5"
                   :exclusions [org.clojure/clojure
                                org.clojure/tools.reader]]
+                 [democracyworks/kehaar "0.5.0"]
                  [javax.servlet/servlet-api "2.5"]
                  [org.immutant/web "2.1.5"
                   :exclusions [commons-codec]]
@@ -35,15 +37,16 @@
                  [com.novemberain/langohr "3.6.1"]
                  [prismatic/schema "1.1.3"]]
   :plugins [[lein-immutant "2.1.0"]]
-  :profiles {:dev {:resource-paths ["dev-resources"]
-                   :source-paths ["dev-src"]}
-             :test {:resource-paths ["test-resources"]
-                    :main usps-processor.core-test}
-             :uberjar {:aot [usps-processor.importer]}
-             :production {:resource-paths ["env-configs/usps-processor/production/resources"]}}
-  :uberjar-name "usps-processor.jar"
+  :main ^:skip-aot usps-processor.core
+  :target-path "target/%s"
   :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
                                    :username :env
                                    :password :env}}
-  :main usps-processor.core
-  :aliases {"reset-db" ["run" "-m" "dev.db"]})
+  :uberjar-name "usps-processor.jar"
+  :profiles {:uberjar {:aot :all}
+             :dev-common {:resource-paths ["dev-resources"]
+                          :source-paths ["dev-src"]}
+             :dev-overrides {}
+             :dev [:dev-common :dev-overrides]
+             :test {:resource-paths ["test-resources"]
+                    :main usps-processor.core-test}})
--- src/usps_processor/core.clj
+++ src/usps_processor/core.clj
@@ -1,7 +1,11 @@
 (ns usps-processor.core
   (:require [usps-processor.importer :as importer]
-            [immutant.util]
-            [clojure.tools.logging :refer [info]])
+            [usps-processor.queue :as queue]
+            [usps-processor.channels :as channels]
+            [datomic-toolbox.core :as dtc]
+            [immutant.util :as immutant]
+            [turbovote.resource-config :refer [config]]
+            [clojure.tools.logging :as log])
   (:gen-class))
 
 (def messages-future (atom nil))
@@ -9,7 +13,7 @@
 (defn start-importer []
   (swap! messages-future (fn [mf]
                            (when mf (future-cancel mf))
-                           (importer/-main)))) ;; returns a new future
+                           (importer/start-message-consumer))))
 
 
 (defn stop-importer []
@@ -19,6 +23,16 @@
                            nil)))
 
 (defn -main [& args]
-  (info "Starting importer thread")
-  (.start (Thread. start-importer))
-  (immutant.util/at-exit stop-importer))
+  (log/info "Starting up...")
+  (cond (config [:datomic :initialize]) (dtc/initialize (config [:datomic]))
+        (config [:datomic :run-migrations]) (do (dtc/configure! (config [:datomic]))
+                                                (dtc/run-migrations)))
+  (log/info "Datomic initialized")
+  (let [rabbit-resources (queue/initialize (config [:rabbitmq :connection]))]
+    (log/info "RabbitMQ initialized")
+    (.start (Thread. start-importer))
+    (log/info "Importer started")
+    (immutant/at-exit (fn []
+                        (queue/close-all! rabbit-resources)
+                        (channels/close-all!)
+                        (stop-importer)))))
