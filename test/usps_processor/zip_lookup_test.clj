99410dd264976fb0a405247a28627d25e93e40c5
Wes Morgan <wesmorgan@icloud.com>
2016-10-31 09:42:04 -0600

    Remove USPS zip->timezone notes in README
    We moved this into the phantom-zone library.

--- test/usps_processor/zip_lookup_test.clj
+++
@@ -1,9 +0,0 @@
-(ns usps-processor.zip-lookup-test
-  (:require [clojure.test :refer :all]
-            [usps-processor.zip-lookup :refer :all]))
-
-(deftest proper-case-test
-  (testing "properly cases a place name"
-    (is (= "Brooklyn" (proper-case "BROOKLYN")))
-    (is (= "New York" (proper-case "NEW YORK")))
-    (is (= "Grand View-on-Hudson" (proper-case "GRAND VIEW-ON-HUDSON")))))
