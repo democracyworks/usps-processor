423e49b776adac0dbba58827bb23df8b45fc98ed
Wes Morgan <wesmorgan@icloud.com>
2017-05-16 11:38:22 -0600

    Update deps

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.clojure/core.async "0.2.395"]
-                 [ch.qos.logback/logback-classic "1.1.8"]
+                 [org.clojure/core.async "0.3.442"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
                  [turbovote.resource-config "0.2.1"]
                  [democracyworks/datomic-toolbox "2.0.4"
                   :exclusions [com.datomic/datomic-pro]]
@@ -16,7 +16,7 @@
                                commons-codec
                                org.jboss.logging/jboss-logging
                                org.apache.httpcomponents/httpcore]]
-                 [com.amazonaws/aws-java-sdk "1.11.6"
+                 [com.amazonaws/aws-java-sdk "1.11.129"
                   :exclusions [commons-codec commons-logging
                                com.fasterxml.jackson.dataformat/jackson-dataformat-cbor
                                com.fasterxml.jackson.core/jackson-core]]
@@ -29,20 +29,20 @@
                  [democracyworks/squishy "3.0.1"
                   :exclusions [commons-codec
                                com.amazonaws/aws-java-sdk-sqs]]
-                 [org.clojure/data.csv "0.1.3"]
+                 [org.clojure/data.csv "0.1.4"]
                  [turbovote.imbarcode "0.1.6"
                   :exclusions [org.clojure/clojure
                                org.clojure/tools.reader
                                org.clojure/clojurescript]]
-                 [democracyworks/kehaar "0.8.1"]
+                 [democracyworks/kehaar "0.10.4"]
                  [javax.servlet/servlet-api "2.5"]
                  [org.immutant/web "2.1.6"
                   :exclusions [commons-codec commons-io]]
                  [com.novemberain/langohr "3.7.0"]
-                 [prismatic/schema "1.1.3"]
+                 [prismatic/schema "1.1.6"]
                  [democracyworks/phantom-zone "0.1.1"]]
   :plugins [[lein-immutant "2.1.0"]
-            [com.carouselapps/jar-copier "0.3.0"]]
+            [com.pupeno/jar-copier "0.4.0"]]
   :java-agents [[com.newrelic.agent.java/newrelic-agent "3.35.1"]]
   :jar-copier {:java-agents true
                :destination "resources/jars"}
